183af19476e2f176dad0b1c43f32457a057a05c6
Natalia Ramirez <natroram@espol.edu.ec>
2021-08-26 13:06:22 -0500

    subiendo tests para Movie y MovieRental

--- RentalSystem2/src/rentalsystem2/MovieRental.java
+++ RentalSystem2/src/rentalsystem2/MovieRental.java
@@ -17,19 +17,6 @@ class MovieRental {
     public Movie getMovie() {
         return _movie;
     }
-    /**
-     * Para las pruebas unitarias
-     * 
-    Movie theManWhoKnewTooMuch, mulan, slumdogMillionaire;
-    @Before
-    public void setUp() {
-        theManWhoKnewTooMuch = new Movie("The Man Who Knew Too Much", Movie.REGULAR);
-        mulan = new Movie("Mulan", Movie.CHILDRENS);
-        slumdogMillionaire = new Movie("Slumdog Millionaire", Movie.NEW_RELEASE);
-    }
-    @Test
-    public void testGetPriceCode() {
-        assertEquals(Movie.REGULAR, theManWhoKnewTooMuch.getPriceCode());
-    }
-     */
+    
+    
 }
